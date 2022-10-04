e3b3050ef153a9209d9d7582d13582801a20ce2f
Shaukat Sohail <65353819+Shaukat456@users.noreply.github.com>
2022-10-04 11:36:08 +0500

    vuecdn

---
+++ app.js
@@ -0,0 +1,15 @@
+const app = Vue.createApp({
+    // template: '<h1> hello </h1>',
+    data(){
+        return {
+      name: {
+        title: "MR",
+        first: "SOME",
+        last: "THING"
+      },
+        }
+    }
+})
+
+
+app.mount('#app')
